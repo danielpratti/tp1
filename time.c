7d34a286c5f23d3bbcc0229a5c6420a887b800cc
danielpratti <kakarotouzu02@gmail.com>
2026-05-20 09:51:29 -0300

    Update time.c

--- time.c
+++ time.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Cria um time com id e nome fornecidos, estatísticas zeradas */
+//Cria um time com id e nome fornecidos, com todos resultados zerados
 Time time_criar(int id, const char *nome) {
     Time t;
     t.id = id;
@@ -16,24 +16,24 @@ Time time_criar(int id, const char *nome) {
     return t;
 }
 
-/* Saldo de gols = GM - GS */
+//Saldo de gols = Gols Marcados - Gols Sofridos
 int time_saldo(const Time *t) {
     return t->gols_marcados - t->gols_sofridos;
 }
 
-/* Pontos ganhos = 3*vitórias + empates */
+//Pontos ganhos = 3*vitórias + empates
 int time_pontos(const Time *t) {
     return 3 * t->vitorias + t->empates;
 }
 
-/* Imprime o cabeçalho da tabela de classificação */
+//Imprime o cabeçalho da tabela de classificação
 void time_imprimir_cabecalho(void) {
     printf("%-4s %-12s %4s %4s %4s %4s %4s %4s %4s\n",
            "ID", "Time", "V", "E", "D", "GM", "GS", "S", "PG");
     printf("----------------------------------------------------\n");
 }
 
-/* Imprime uma linha da tabela com as estatísticas do time */
+//Imprime uma linha da tabela com as estatísticas do time
 void time_imprimir(const Time *t) {
     printf("%-4d %-12s %4d %4d %4d %4d %4d %4d %4d\n",
            t->id, t->nome,
